f4ddc81bf2b82a1576492ba10532d9d107bcb6ae
mbrenman <mattbrenman@gmail.com>
2014-04-20 21:40:19 -0400

    changed to mongoose backend to add easy login/ user auth

--- sharingIsCaring/app.js
+++ sharingIsCaring/app.js
@@ -1,106 +1,158 @@
-
-/**
- * Module dependencies.
- */
-var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
-var http = require('http');
+// dependencies
 var path = require('path');
+var express = require('express');
+var http = require('http');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
-var mongo = require('mongodb');
-
-
+// main config
 var app = express();
-
-var mongoUri = process.env.MONGOLAB_URI ||
-  process.env.MONGOHQ_URL ||
-  'mongodb://localhost/local';
-
-// all environments
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
+app.set('view options', { layout: false });
+app.use(express.logger());
+app.use(express.bodyParser());
 app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+app.configure('development', function(){
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
 
-app.all('*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
- });
+app.configure('production', function(){
+    app.use(express.errorHandler());
+});
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+// passport config
+var Account = require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
+var db = mongoose.connection;
+
+// mongoose
+mongoose.connect('mongodb://localhost/local');
+
+// var itemSchema = new mongoose.Schema({
+// 	member: String, 
+// 	quantity: Number 
+// });
+
+// var supplySchema = new mongoose.Schema({
+// 	supplies: 
+// 	{
+// 		item: itemSchema
+// 	}
+// });
+
+// var groupSchema = new mongoose.Schema({
+//   name: String
+// , supplies: supplySchema
+// });
+
+var groupSchema = new mongoose.Schema({
+	name: String,
+	supplies: { type: mongoose.Schema.Types.Mixed }
+});
+
+var Group = mongoose.model('Group', groupSchema);
+
+// routes
+require('./routes')(app);
 
 app.post('/newGroup.json', function (req, res){
-  mongo.Db.connect(mongoUri, function (err, db){
-    db.collection("groups", function (er, collection){
+  // mongo.Db.connect(mongoUri, function (err, db){
+    // db.collection("groups", function (er, collection){
     	console.log('\n\n\n' + req.body + '\n\n\n');
 		var groupname = req.body.groupname;
 		var members = req.body.members;
 		var items = req.body.items;
-		var supplies = {}
+		var supplylist = {}
 		for(var i=0; i<items.length; i++){
 			var itemname = items[i];
-			supplies[itemname] = {}
+			supplylist[itemname] = {}
 			for(var j=0; j<members.length; j++){
 				var uname = members[j];
-				supplies[itemname][uname] = 0;
+				supplylist[itemname][uname] = 0;
 			}
 		}
-		console.log(supplies);
+
 		var data = {};
 		data["groupname"] = groupname;
-		data["supplies"] = supplies;
-		collection.insert(data, function (err, r){});
+		data["supplies"] = supplylist;
+
+		var d = new Group({
+			name: groupname,
+			supplies: supplylist
+		});
+
+		d.save(function(err, r) {
+ 			if (err) return console.error(err);
+  			console.dir(r);
+		});
+		// console.log(supplies);
+
+		// collection.insert(data, function (err, r){});
         res.send("Successful entry!\n");
-		// console.log(groupname);
-		// console.log(members);
-		// console.log(items);
-		res.send('success!');
-	});
-  });
+	// });
+  // });
 });
 
 app.post('/incrementItem.json', function(req, res){
-	mongo.Db.connect(mongoUri, function (err, db){
-		db.collection("groups", function (er, col){
-			var groupname = req.body.groupname;
-			var name = req.body.name;
-			var item = req.body.item;
-			console.log(groupname);
-			col.find({groupname : groupname}).toArray(function(e, x){
-				var newGroup = {
-					groupname: groupname,
-					supplies: x[0]["supplies"]
-				};
-				console.log(x[0]);
-				console.log(item);
-				console.log(name);
-				console.log(newGroup["supplies"][item][name])
-				
-				var quantity = parseInt(newGroup["supplies"][item][name]);
-				newGroup["supplies"][item][name] = quantity + 1;
-				console.log(x[0]["_id"]);
-				col.remove({groupname : groupname}, function (error, result){});
-				col.insert(newGroup, function (err, r){});
-	        	res.send("Successful Update!\n");
-			});
+	var groupname = req.body.groupname;
+	var name = req.body.name;
+	var item = req.body.item;
+	Group.findOne({ name: groupname }, function(err, r) {
+  		if (err) return console.error(err);
+  		console.log(r);
+  		new_doc = new Group({
+  			name: r.name,
+  			supplies: r.supplies
+  		});
+  		new_doc.supplies[item][name] += 1;
+  		new_doc.save(function(err, r) {
+ 			if (err) return console.error(err);
+  			console.dir(r);
 		});
+		r.remove();
+  		res.send("Updated!\n");
 	});
+
+	// mongo.Db.connect(mongoUri, function (err, db){
+	// 	db.collection("groups", function (er, col){
+	// 		var groupname = req.body.groupname;
+	// 		var name = req.body.name;
+	// 		var item = req.body.item;
+	// 		console.log(groupname);
+	// 		col.find({groupname : groupname}).toArray(function(e, x){
+	// 			var newGroup = {
+	// 				groupname: groupname,
+	// 				supplies: x[0]["supplies"]
+	// 			};
+	// 			console.log(x[0]);
+	// 			console.log(item);
+	// 			console.log(name);
+	// 			console.log(newGroup["supplies"][item][name])
+				
+	// 			var quantity = parseInt(newGroup["supplies"][item][name]);
+	// 			newGroup["supplies"][item][name] = quantity + 1;
+	// 			console.log(x[0]["_id"]);
+	// 			col.remove({groupname : groupname}, function (error, result){});
+	// 			col.insert(newGroup, function (err, r){});
+	//         	res.send("Successful Update!\n");
+	// 		});
+	// 	});
+	// });
 });
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+app.listen(app.get('port'), function(){
+  console.log(("Express server listening on port " + app.get('port')))
 });
--- sharingIsCaring/routes/index.js
+++ sharingIsCaring/routes/index.js
@@ -1,8 +1,9 @@
-
-/*
- * GET home page.
- */
+var path = require("path");
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Sharing is Caring' });
+  res.render('index', { title: "Passport-Examples"});
+};
+
+exports.ping = function(req, res){
+  res.send("pong!", 200);
 };
